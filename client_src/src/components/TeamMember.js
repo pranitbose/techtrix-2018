d34b5e87e9d315c0760e5c79d42e45953aea8c89
arpanpathak <arpanpathak1996@gmail.com>
2018-01-11 17:12:41 +0530

    preloader added, cards animation added,box-shadow on cards changes,chip and tape added on card,

--- client_src/src/components/TeamMember.js
+++ client_src/src/components/TeamMember.js
@@ -3,17 +3,17 @@ import React from 'react';
 const TeamMember = (props) => (
   <div className='teamMembers'>
       {props.members.map((member) =>
-        <div key={member.m_id} className='card'>
-          <div className='card-image'>
+        <div key={member.m_id} className='card spin tape'>
+          <div className='card-image z-depth-4'>
             <img src={require(`../assets/images/team/${member.name.split(' ')[0].toLowerCase()}.jpg`)} alt="Team Member Image" />
             <a href={`${member.socialLink}`} className='btn-floating halfway-fab waves-effect waves-light light-blue darken-4' rel="noopener noreferrer" target="_blank">
               <i className='fa fa-facebook white-text'></i>
             </a>
           </div>
           <div className='card-content'>
-            <p className='card-title grey-text text-darken-4'>{member.name}</p>
+            <p className='card-title grey-text text-lighten-4 chip cyan darken-4'>{member.name}</p>
             <hr />
-            <p className='grey-text text-darken-4'>{member.position}</p>
+            <p className='cyan-text text-darken-4'>{member.position}</p>
             <br />
             <div className="contact-info">
               <p><a href={`tel:${member.contactNum}`} className='grey-text text-darken-4'><i className='material-icons black-text left'>phone</i>{member.contactNum}</a></p>
